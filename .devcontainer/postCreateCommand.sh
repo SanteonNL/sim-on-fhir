43a344a33a67d9c8ddc59d6be7e93020b86322f3
Bram Wesselo <b.wesselo@santeon.nl>
2023-12-06 09:32:42 +0000

    devcontainer working with java fonts

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Hello world from postCreateCommand.sh"
+echo "Hello from postCreateCommand.sh"
 
-dotnet tool list
-
+echo "Installing Firely Terminal"
 dotnet tool install -g firely.terminal
 
-# sudo apt-get install libfreetype6
-# sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so.6.10.1 /usr/lib/libfreetype.so.6
-# sudo apt-get install libfreetype6
-# sudo apt-get -y install fontconfig
+echo "Installing fonts for Java FHIR-publisher"
+sudo apt-get update
+sudo apt-get -y install libfreetype6 fontconfig
 
+echo "Installing/downloading Java FHIR-publisher"
 sudo ./_updatePublisher.sh -y
 
-mkdir bin
-
+echo "Downloading protobuf-scripts to generate proto-files from FHIR-profiles"
+mkdir -p bin
 curl https://raw.githubusercontent.com/google/fhir/v0.5.0/bazel/generate_protos_utils.sh -o ./bin/generate_protos_utils.sh     && \
   curl https://raw.githubusercontent.com/google/fhir/v0.5.0/bazel/generate_protos.sh -o ./bin/generate_protos.sh && \
   curl https://raw.githubusercontent.com/google/fhir/v0.5.0/bazel/generate_definitions_and_protos.sh -o ./bin/generate_definitions_and_protos.sh && \
